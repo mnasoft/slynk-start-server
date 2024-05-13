dfc9d6a41c5d1db6ad28a0879d1c3cd8c7c54faf
Mykola Matvyeyev <mnasoft@gmail.com>
2024-05-13 10:15:45 +0300

    Mon May 13 07:15:45 UTC 2024

--- slynk-start-server.lisp
+++ slynk-start-server.lisp
@@ -1,3 +1,7 @@
+;; (push #p"~/dir/to/sly" asdf:*central-registry*)
+;; (asdf:load-system :slynk)
+;; (slynk:create-server :port 4008)
+
 (ql:quickload :slynk)
 (slynk:create-server :port 4005 :style :spawn :dont-close t)
-
+;;(slynk:create-server :port 4005)
