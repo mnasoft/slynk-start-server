68eea52afcff8a15c5dd91532d45459613704b7e
Mykola Matvyeyev <mnasoft@gmail.com>
2025-06-08 21:12:37 +0300

    Вс 08 июн 2025 18:12:37 UTC

--- slynk-start-ecl.lisp
+++ slynk-start-ecl.lisp
@@ -1,3 +1,3 @@
 (load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
 (ql:quickload :slynk)
-(slynk:create-server :port 4005 :style :spawn :dont-close t)
+(slynk:create-server :port 4006 :style :spawn :dont-close t)
