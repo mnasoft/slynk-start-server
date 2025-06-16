b080e57fe0ce81cedc5edaabc6dfde7aaba885bf
Mykola Matvyeyev <mnasoft@gmail.com>
2025-06-16 07:44:23 +0300

    Пн 16 июн 2025 04:44:23 UTC

--- slynk-start-sbcl.lisp
+++ slynk-start-sbcl.lisp
@@ -3,7 +3,10 @@
 (slynk:create-server :port 4005 :style :spawn :dont-close t)
 
 ;; Здесь выполняются дополнительные команды после запуска сервера
-;;(format t "Slynk запущен!~%")
-;;(ql:quickload :vse-gost/web)
-;;(vse-gost/web:start-gosts)
+(sleep 5)
+(format t "Slynk запущен!~%")
+(ql:quickload :vse-gost/web)
+(format t ":vse-gost/web - loaded!~%")
+(vse-gost/web:start-gosts)
+(format t "vse-gost/web:start-gosts!~%")
 
